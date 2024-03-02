77f96e9c5b16e9c520a398ec824c240b8f3fa557
icyiah <isaiahngyz@gmail.com>
2024-03-02 16:19:24 +0800

    test screen

---
+++ zmk-split-battery-master/BatteryMonitor.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Storage.Streams;
+using System.Diagnostics;
+
+namespace ZMKSplit
+{
+    internal class BatteryMonitor
+    {
+        public static readonly Guid BATTERY_UUID = Guid.Parse("{0000180F-0000-1000-8000-00805F9B34FB}");
+        public static readonly Guid BATTERY_LEVEL_UUID = Guid.Parse("{00002A19-0000-1000-8000-00805F9B34FB}");
+        public static readonly string BATTERY_DEFAULT_NAME = "Main";
+
+        public delegate void ListDevicesCallback(string deviceName, string deviceID);
+        public delegate void ListDevicesCompletionCallback();
+        public delegate void BatteryLevelChangedCallback();
+
+        public struct BatteryStatus
+        {
+            public string Name { get; set; }
+            public int Level { get; set; }
+        };
+
+        public enum ConnectStatus
+        {
+            Connected,
+            DeviceNotFound,
+            BatteryServiceNotFound,
+            BatteryLevelCharacteristicNotFound,
+            SubscribtionFailure,
+        }
+
+        public struct ConnectResult
+        {
+            public ConnectStatus Status { get; set; }
+            public string ErrorMessage { get; set; }
+        }
+
+        public enum ReadStatus
+        {
+            Success,
+            NotConnected,
+            Failure,
+        }
+
+        public struct ReadBatteryLevelResult
+        {
+            public ReadStatus Status { get; set; }
+            public Dictionary<ushort, BatteryStatus> Batteries { get; set; }
+            public string ErrorMessage { get; set; }
+
+            public ReadBatteryLevelResult(ReadStatus status, GattCharacteristic gc, int singleValue) : this(status, "")
+            {
+                Batteries[gc.AttributeHandle] = new BatteryStatus { Name = GetBatteryNameFromGC(gc), Level = singleValue };
+            }
+
+            public ReadBatteryLevelResult(ReadStatus status, string errorMessage)
+            {
+                Status = status;
+                Batteries = new Dictionary<ushort, BatteryStatus>();
+                ErrorMessage = errorMessage;
+            }
+        }
+
+        private class BLEDevice
+        {
+            public string Name { get; set; }
+            public BluetoothLEDevice Device { get; set; }
+            public IReadOnlyList<GattCharacteristic> GattCharacteristics { get; set; }
+
+            public BLEDevice(string name, BluetoothLEDevice dev, IReadOnlyList<GattCharacteristic> gattChrs)
+            {
+                Name = name;
+                Device = dev;
+                GattCharacteristics = gattChrs;
+            }
+        };
+
+        public Dictionary<ushort, BatteryStatus> Batteries { get => _batteries; }
+
+        private Dictionary<ushort, BatteryStatus> _batteries = new();
+        private BLEDevice? _bleDevice;
+        private readonly BatteryLevelChangedCallback _batteryLevelChangedCb;
+
+        public BatteryMonitor(BatteryLevelChangedCallback cb)
+        {
+            _batteryLevelChangedCb = cb;
+        }
+        
+        public static void ListPairedDevices(ListDevicesCallback cb, ListDevicesCompletionCallback completionCB)
+        {
+            string aqsFilter = "(System.Devices.Aep.ProtocolId:=\"{bb7bb05e-5972-42b5-94fc-76eaa7084d49}\")";
+            string[] bleAdditionalProperties =
+            {
+                "System.Devices.Aep.DeviceAddress",
+                "System.Devices.Aep.Bluetooth.Le.IsConnectable",
+            };
+
+            DeviceWatcher watcher = DeviceInformation.CreateWatcher(aqsFilter, bleAdditionalProperties, DeviceInformationKind.AssociationEndpoint);
+            watcher.Added += (DeviceWatcher deviceWatcher, DeviceInformation di) =>
+            {
+                if (di.Pairing.IsPaired && !String.IsNullOrWhiteSpace(di.Name))
+                {
+                    cb(di.Name, di.Id);
+                }
+            };
+            watcher.Updated += (_, __) => { };
+            watcher.EnumerationCompleted += (DeviceWatcher deviceWatcher, object arg) =>
+            {
+                deviceWatcher.Stop();
+            };
+            watcher.Stopped += (DeviceWatcher deviceWatcher, object arg) =>
+            {
+                completionCB();
+            };
+            watcher.Start();
+        }
+        
+        public async Task<ConnectResult> Connect(string deviceName, string deviceID)
+        {
+            Disconnect();
+            var dev = await BluetoothLEDevice.FromIdAsync(deviceID);
+            if (dev == null)
+            {
+                return new ConnectResult { Status = ConnectStatus.DeviceNotFound };
+            }
+            
+            var gattServices = await dev.GetGattServicesForUuidAsync(BATTERY_UUID, BluetoothCacheMode.Uncached).AsTask();
+            if (gattServices == null)
+            {
+                return new ConnectResult { Status = ConnectStatus.BatteryServiceNotFound };
+            }
+
+            var allCharacteristics = new List<GattCharacteristic>();
+            for (int i = 0; i < gattServices.Services.Count; i++)
+            {
+                var gattService = gattServices.Services[i];
+                var gattCharacteristics = await gattService.GetCharacteristicsForUuidAsync(BATTERY_LEVEL_UUID, BluetoothCacheMode.Uncached);
+
+                foreach (var gc in gattCharacteristics!.Characteristics)
+                {
+                    _batteries[gc.AttributeHandle] = new BatteryStatus { Name = GetBatteryNameFromGC(gc), Level = -1 };
+                    gc.ValueChanged += OnGattValueChanged;
+                    allCharacteristics.Add(gc);
+                }
+            }
+
+            if (_batteries.Count != 0)
+            {
+                _bleDevice = new BLEDevice(deviceName, dev, allCharacteristics);
+
+                var readResult = await ReadBatteryLevels();
+                if (readResult.Status == ReadStatus.Success)
+                {
+                    _batteries = readResult.Batteries;
+                }
+
+                return new ConnectResult { Status = ConnectStatus.Connected };
+            }
+            else
+            {
+                return new ConnectResult { Status = ConnectStatus.BatteryLevelCharacteristicNotFound };
+            }
+        }
+
+        public void Disconnect()
+        {
+            _batteries.Clear();
+            if (_bleDevice != null)
+            {
+                foreach (var gc in _bleDevice.GattCharacteristics)
+                {
+                    gc.ValueChanged -= OnGattValueChanged;
+                }
+                _bleDevice.Device.Dispose();
+                _bleDevice = null;
+            }
+        }
+
+        public bool IsConnected()
+        {
+            return _bleDevice != null;
+        }
+
+        private static string GetBatteryNameFromGC(GattCharacteristic gc)
+        {
+            if (string.IsNullOrWhiteSpace(gc.UserDescription))
+            {
+                return BATTERY_DEFAULT_NAME;
+            }
+            else
+            {
+                return gc.UserDescription;
+            }
+        }
+
+        private static async Task<ReadBatteryLevelResult> ReadBatteryLevel(GattCharacteristic gc)
+        {
+            try
+            {
+                var res = await gc.ReadValueAsync(BluetoothCacheMode.Uncached);
+                if (res == null)
+                {
+                    return new ReadBatteryLevelResult(ReadStatus.Failure, "ReadValueAsync returned null");
+                }
+
+                if (res.Status != GattCommunicationStatus.Success)
+                {
+                    return new ReadBatteryLevelResult(ReadStatus.Failure, "ReadValueAsync returned status " + res.Status.ToString());
+                }
+
+                IBuffer buffer = res.Value;
+                byte[] data = new byte[buffer.Length];
+                DataReader.FromBuffer(buffer).ReadBytes(data);
+                int lvl = data[0] == 255 ? -1 : data[0];
+                return new ReadBatteryLevelResult(ReadStatus.Success, gc, lvl);
+            }
+            catch (Exception e)
+            {
+                return new ReadBatteryLevelResult(ReadStatus.Failure, e.Message);
+            }
+        }
+
+        public async Task<ReadBatteryLevelResult> ReadBatteryLevels()
+        {
+            if (_bleDevice == null)
+                return new ReadBatteryLevelResult { Status = ReadStatus.NotConnected }; ;
+
+            var bleDev = _bleDevice;
+            var overalResult = new ReadBatteryLevelResult(ReadStatus.Success, "");
+            foreach (var gc in bleDev.GattCharacteristics)
+            {
+                var res = await ReadBatteryLevel(gc);
+                if (res.Status != ReadStatus.Success)
+                    return res;
+                overalResult.Batteries.Add(gc.AttributeHandle, res.Batteries[gc.AttributeHandle]);
+            }
+            return overalResult;
+        }
+
+        public void OnGattValueChanged(GattCharacteristic sender, GattValueChangedEventArgs args)
+        {
+            byte[] data = new byte[args.CharacteristicValue.Length];
+            DataReader.FromBuffer(args.CharacteristicValue).ReadBytes(data);
+            int lvl = data[0] == 255 ? -1 : data[0];
+            _batteries[sender.AttributeHandle] = new BatteryStatus { Name = GetBatteryNameFromGC(sender), Level = lvl };
+            _batteryLevelChangedCb();
+        }
+    }
+}
---
+++ zmk-split-battery-master/MainForm.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace ZMKSplit
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            DevicesListView = new System.Windows.Forms.ListView();
+            NameColumn = new System.Windows.Forms.ColumnHeader();
+            NotifyIcon = new System.Windows.Forms.NotifyIcon(components);
+            TrayContextMenu = new System.Windows.Forms.ContextMenuStrip(components);
+            showContextMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            exitContextMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ReloadButton = new System.Windows.Forms.Button();
+            StatusStrip1 = new System.Windows.Forms.StatusStrip();
+            StatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            ConnectButton = new System.Windows.Forms.Button();
+            ReconnectTimer = new System.Windows.Forms.Timer(components);
+            AutoRunCheckBox = new System.Windows.Forms.CheckBox();
+            TrayContextMenu.SuspendLayout();
+            StatusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // DevicesListView
+            // 
+            DevicesListView.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            DevicesListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] { NameColumn });
+            DevicesListView.FullRowSelect = true;
+            DevicesListView.GridLines = true;
+            DevicesListView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            DevicesListView.HideSelection = false;
+            DevicesListView.Location = new System.Drawing.Point(12, 12);
+            DevicesListView.MultiSelect = false;
+            DevicesListView.Name = "DevicesListView";
+            DevicesListView.ShowGroups = false;
+            DevicesListView.Size = new System.Drawing.Size(382, 103);
+            DevicesListView.TabIndex = 0;
+            DevicesListView.UseCompatibleStateImageBehavior = false;
+            DevicesListView.View = System.Windows.Forms.View.Details;
+            DevicesListView.DoubleClick += DevicesListView_DoubleClick;
+            // 
+            // NameColumn
+            // 
+            NameColumn.Text = "Name";
+            NameColumn.Width = 378;
+            // 
+            // NotifyIcon
+            // 
+            NotifyIcon.ContextMenuStrip = TrayContextMenu;
+            NotifyIcon.Visible = true;
+            NotifyIcon.MouseDoubleClick += NotifyIcon_MouseDoubleClick;
+            // 
+            // TrayContextMenu
+            // 
+            TrayContextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { showContextMenuItem, exitContextMenuItem });
+            TrayContextMenu.Name = "trayContextMenu";
+            TrayContextMenu.Size = new System.Drawing.Size(104, 48);
+            // 
+            // showContextMenuItem
+            // 
+            showContextMenuItem.Name = "showContextMenuItem";
+            showContextMenuItem.Size = new System.Drawing.Size(103, 22);
+            showContextMenuItem.Text = "Show";
+            showContextMenuItem.Click += ShowContextMenuItem_Click;
+            // 
+            // exitContextMenuItem
+            // 
+            exitContextMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            exitContextMenuItem.Name = "exitContextMenuItem";
+            exitContextMenuItem.Size = new System.Drawing.Size(103, 22);
+            exitContextMenuItem.Text = "E&xit";
+            exitContextMenuItem.Click += ExitContextMenuItem_Click;
+            // 
+            // ReloadButton
+            // 
+            ReloadButton.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
+            ReloadButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            ReloadButton.Location = new System.Drawing.Point(407, 12);
+            ReloadButton.Name = "ReloadButton";
+            ReloadButton.Size = new System.Drawing.Size(158, 32);
+            ReloadButton.TabIndex = 3;
+            ReloadButton.Text = "Refresh";
+            ReloadButton.UseVisualStyleBackColor = true;
+            ReloadButton.MouseClick += ReloadButton_MouseClick;
+            // 
+            // StatusStrip1
+            // 
+            StatusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { StatusLabel });
+            StatusStrip1.Location = new System.Drawing.Point(0, 118);
+            StatusStrip1.Name = "StatusStrip1";
+            StatusStrip1.Size = new System.Drawing.Size(577, 22);
+            StatusStrip1.TabIndex = 4;
+            StatusStrip1.Text = "statusStrip1";
+            // 
+            // StatusLabel
+            // 
+            StatusLabel.Name = "StatusLabel";
+            StatusLabel.Size = new System.Drawing.Size(39, 17);
+            StatusLabel.Text = "Ready";
+            // 
+            // ConnectButton
+            // 
+            ConnectButton.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
+            ConnectButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            ConnectButton.Location = new System.Drawing.Point(407, 50);
+            ConnectButton.Name = "ConnectButton";
+            ConnectButton.Size = new System.Drawing.Size(158, 32);
+            ConnectButton.TabIndex = 5;
+            ConnectButton.Text = "Connect";
+            ConnectButton.UseVisualStyleBackColor = true;
+            ConnectButton.Click += ConnectButton_Click;
+            // 
+            // ReconnectTimer
+            // 
+            ReconnectTimer.Interval = 1000;
+            ReconnectTimer.Tick += ReconnectTimer_Tick;
+            // 
+            // AutoRunCheckBox
+            // 
+            AutoRunCheckBox.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
+            AutoRunCheckBox.AutoSize = true;
+            AutoRunCheckBox.Location = new System.Drawing.Point(408, 88);
+            AutoRunCheckBox.Name = "AutoRunCheckBox";
+            AutoRunCheckBox.Size = new System.Drawing.Size(152, 19);
+            AutoRunCheckBox.TabIndex = 6;
+            AutoRunCheckBox.Text = "Run at Windows startup";
+            AutoRunCheckBox.UseVisualStyleBackColor = true;
+            AutoRunCheckBox.CheckedChanged += AutoRunCheckBox_CheckedChanged;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(577, 140);
+            Controls.Add(AutoRunCheckBox);
+            Controls.Add(ConnectButton);
+            Controls.Add(StatusStrip1);
+            Controls.Add(ReloadButton);
+            Controls.Add(DevicesListView);
+            Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+            Name = "MainForm";
+            Text = "ZMK Split Battery Status";
+            FormClosing += MainForm_FormClosing;
+            Load += MainForm_Load;
+            TrayContextMenu.ResumeLayout(false);
+            StatusStrip1.ResumeLayout(false);
+            StatusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView DevicesListView;
+        private System.Windows.Forms.NotifyIcon NotifyIcon;
+        private System.Windows.Forms.Button ReloadButton;
+        private System.Windows.Forms.ColumnHeader NameColumn;
+        private System.Windows.Forms.StatusStrip StatusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.ContextMenuStrip TrayContextMenu;
+        private System.Windows.Forms.ToolStripMenuItem exitContextMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showContextMenuItem;
+        private System.Windows.Forms.Timer ReconnectTimer;
+        private System.Windows.Forms.CheckBox AutoRunCheckBox;
+    }
+}
